8ea5b7479ecfd69bd1440388e5538f599de86db8
haku <hakan_uysal@ymail.com>
2015-06-12 14:54:17 +0100

    Fixed firebase backend.

--- site.js
+++ site.js
@@ -102,16 +102,29 @@ $(document).ready(function (){
     }
   });
 
+  var params = getJsonFromUrl(window.location.href);
+  console.log(JSON.stringify(params));
+  if (params && params['q']) {
+    getDertById(params['q']);
+  }
+
+  getLastDerts();
+
 });
 
 function saveDert(dert) {
   console.log('Saving dert: ' + dert);
+
   jQuery.ajax({
-      type: 'POST',
+      accept: "application/json",
+      method: "POST",
+      type: "POST",
+      contentType: "application/json; charset=utf-8",
+      // dataType: "jsonp",
       url: "https://glowing-heat-3755.firebaseio.com/dert.json",
-      data: {"dert": dert, "tarih": new Date()},
-      success: function successfulSaving() {
-        console.log('Saved successfully...');
+      data: JSON.stringify({"dert": dert, "tarih": new Date()}),
+      success: function successfulSaving(data) {
+        console.log('Saved successfully...' + JSON.stringify(data));
         $("#p-dert-message")[0].innerHTML = "Derdin başarıyla sevildi";
       },
       fail: function failSaving() {
@@ -119,4 +132,52 @@ function saveDert(dert) {
         $("#p-dert-message")[0].innerHTML = "Dert sevme hatasi";
       }
   });
+}
+
+function getLastDerts() {
+  jQuery.ajax({
+      url: "https://glowing-heat-3755.firebaseio.com/dert.json?orderBy=%22tarih%22&limitToLast=10",
+      success: function successfulSaving(data) {
+        console.log('Got derts: ' + JSON.stringify(data));
+        appendDertsToPage(data);
+      },
+      fail: function failSaving() {
+        console.log('Smt wrong...');
+      }
+  })
+}
+
+function getDertById(id) {
+  jQuery.ajax({
+      url: "https://glowing-heat-3755.firebaseio.com/dert/" + id + ".json",
+      success: function successfulSaving(data) {
+        console.log('Got derts: ' + JSON.stringify(data));
+        appendOneDertToPage(data);
+      },
+      fail: function failSaving() {
+        console.log('Smt wrong...');
+      }
+  })
+}
+
+
+
+function appendDertsToPage(lastDerts) {
+  for (var i in lastDerts) {
+    $('.ul-last-derts').append($('<li><a href="/?q='+ i + '"> ' + lastDerts[i]['dert'] + '</a></li>'));
+  }
+}
+
+function appendOneDertToPage(dert) {
+  $('#h4-one-dert').innerHTML = dert['dert'];
+}
+
+function getJsonFromUrl() {
+  var query = location.search.substr(1);
+  var result = {};
+  query.split("&").forEach(function(part) {
+    var item = part.split("=");
+    result[item[0]] = decodeURIComponent(item[1]);
+  });
+  return result;
 }
